860e5a14e2943a80fc0432c2d4e39f9d6400aac4
vir <virs98@outlook.com>
2024-07-30 17:59:08 +0800

    chore: :necktie: 编辑器增加参数

--- src/Editor/index.md
+++ src/Editor/index.md
@@ -6,19 +6,28 @@ group:
   title: 数据录入
 ---
 
-# 编辑器
+在 `BlockNote` 基础上开发，封装操作
 
-## 基础的编辑器
+## 基础编辑器
 
-### 默认内容块
+#### 默认内容块
 
 ```jsx
 import { Editor } from 'zs_library';
 
-export default () => <Editor />;
+export default () => (
+  <Editor
+    value={[
+      {
+        type: 'paragraph',
+        content: 'Hello World!!!',
+      },
+    ]}
+  />
+);
 ```
 
-### 隐藏内容块
+#### 隐藏内容块
 
 ```jsx
 import { Editor } from 'zs_library';
@@ -28,12 +37,31 @@ export default () => <Editor hideSpecs={['image', 'video', 'audio', 'file']} />;
 
 ## 新增的内容块
 
-### 代码块
+#### 代码块
 
 使用 `Ace Editor` 作为 代码块组件
 
 - Monaco Editor (不支持自动高度，需要额外设置)
 - CodeMirror (输入与 BlockNote 冲突)
 - Ace Editor
+
+```jsx
+import { Editor } from 'zs_library';
+
+export default () => (
+  <Editor
+    value={[
+      {
+        type: 'codeBlock',
+        content: '// Hello World\n\nconsole.log("Hello World !!!");',
+        props: {
+          language: 'typescript',
+          theme: 'github_dark',
+        },
+      },
+    ]}
+  />
+);
+```
 
 <API id="Editor" />
--- src/Editor/index.tsx
+++ src/Editor/index.tsx
@@ -1,34 +1,38 @@
 import {
+  BlockNoteEditor,
   BlockNoteSchema,
   defaultBlockSpecs,
   filterSuggestionItems,
   insertOrUpdateBlock,
   locales,
+  PartialBlock,
 } from '@blocknote/core';
 import '@blocknote/core/fonts/inter.css';
 import { BlockNoteView } from '@blocknote/mantine';
 import '@blocknote/mantine/style.css';
 import {
-  SuggestionMenuController,
   getDefaultReactSlashMenuItems,
-  useCreateBlockNote,
+  SuggestionMenuController,
 } from '@blocknote/react';
 import { MantineProvider } from '@mantine/core';
 import { RiCodeLine } from '@remixicon/react';
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import { Code } from './Blocks/Code';
 
 export type BlockNoteViewProps = React.ComponentProps<typeof BlockNoteView>;
 
-export interface EditorProps extends Partial<BlockNoteViewProps> {
-  editor?: ReturnType<typeof useCreateBlockNote>;
+export interface EditorProps
+  extends Omit<Partial<BlockNoteViewProps>, 'onChange'> {
   /** 希望隐藏的内容块 */
   hideSpecs?: (keyof typeof defaultBlockSpecs)[];
+  /** 由于添加了自定义块，导致类型复杂无法在此处设置 typeof schema.BlockNoteEditor['initialContent'] */
+  value?: PartialBlock<any>[];
+  onChange?: (value: PartialBlock<any>[]) => void;
 }
 
 const Editor: FC<EditorProps> = (props) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { editor: propEditor, hideSpecs = [], ...rest } = props;
+  const { hideSpecs = [], value, onChange, ...rest } = props;
 
   const schema = BlockNoteSchema.create({
     blockSpecs: {
@@ -58,21 +62,26 @@ const Editor: FC<EditorProps> = (props) => {
     icon: <RiCodeLine size={18} />,
   });
 
-  const editor = useCreateBlockNote({
-    schema,
-    dictionary: locales.zh,
-  });
-
-  const {} = editor;
+  const editor = useMemo(() => {
+    return BlockNoteEditor.create({
+      // @ts-ignore
+      initialContent: value,
+      schema,
+      dictionary: locales.zh,
+    });
+  }, [value]);
 
   return (
     <div>
       <MantineProvider>
         <BlockNoteView
+          // @ts-ignore
           editor={editor}
           slashMenu={false}
+          onChange={() => {
+            onChange?.(editor.document);
+          }}
           {...rest}
-          // onKeyDown={handleKeyDown}
         >
           <SuggestionMenuController
             triggerCharacter={'/'}
