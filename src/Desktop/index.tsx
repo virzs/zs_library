8e0a97515d9bc4b60131c344fa8ade40c35c8c25
vir <virs98@outlook.com>
2024-08-15 11:23:05 +0800

    chore: :bulb: 添加参数说明

--- src/Desktop/index.tsx
+++ src/Desktop/index.tsx
@@ -5,10 +5,26 @@ import { Theme } from './theme';
 import { SortItem } from './types';
 
 export interface DesktopProps<D = any, C = any> extends SortableProps<D, C> {
+  /**
+   * 数据源
+   */
   list: SortItem<D, C>[];
+  /**
+   * onChange 事件，当排序发生变化时触发
+   */
   onChange?: (list: SortItem<D, C>[]) => void;
+  /**
+   * 本地存储 key
+   */
   readonly storageKey?: string;
+  /**
+   * 主题配置
+   */
   theme?: 'light' | 'dark' | Theme;
+  /**
+   * 是否启用缓存
+   * @default true
+   */
   enableCaching: boolean;
 }
 
