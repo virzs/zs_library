d00b119b6852dbe5dfd03e311f6aa95a9adf141b
vir <virs98@outlook.com>
2024-09-19 17:44:01 +0800

    chore(config): :wrench: 修改站点配置

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,8 +1,10 @@
-import { pluginPreview } from '@rspress/plugin-preview';
-import { defineConfig } from 'rspress/config';
+import { pluginPreview } from "@rspress/plugin-preview";
+import { defineConfig } from "rspress/config";
 
 export default defineConfig({
   // 文档根目录
-  root: 'docs',
+  root: "docs",
+  title: "zs_library",
+  description: "个人业务相关的组件库",
   plugins: [pluginPreview()],
 });
