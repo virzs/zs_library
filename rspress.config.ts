be0ae5aee7edd913685f8620107ffc269e07e0f4
vir <virs98@outlook.com>
2024-12-09 17:12:15 +0800

    fix: :bug: 修复构建异常

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,8 +1,6 @@
 import { pluginPreview } from "@rspress/plugin-preview";
 import { defineConfig } from "rspress/config";
-import { pluginTypeDoc } from "@rspress/plugin-typedoc";
 import { pluginApiDocgen } from "@rspress/plugin-api-docgen";
-import path from "path";
 
 export default defineConfig({
   // 文档根目录
@@ -11,14 +9,6 @@ export default defineConfig({
   description: "个人业务相关的组件库",
   plugins: [
     pluginPreview(),
-    pluginTypeDoc({
-      entryPoints: [
-        path.join(__dirname, "src", "components", "md-editor", "index.tsx"),
-        path.join(__dirname, "src", "components", "md-editor", "editor.tsx"),
-        path.join(__dirname, "src", "components", "md-editor", "preview.tsx"),
-        path.join(__dirname, "src", "components", "desktop", "index.tsx"),
-      ],
-    }),
     pluginApiDocgen({
       entries: {
         editor: "./src/components/md-editor/editor.tsx",
