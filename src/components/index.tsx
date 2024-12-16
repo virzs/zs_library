57c29f822beac95ce594c03757549cbdade31952
vir <virs98@outlook.com>
2024-12-16 10:54:42 +0800

    feat(Markdown): :sparkles: 增加 MdEditor Preview 组件单独导出

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Desktop } from "./desktop";
 export { default as MdEditor } from "./md-editor";
+export { default as Markdown } from "./md-editor/preview";
 export { default as Dock } from "./dock";
