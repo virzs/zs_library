8b1838eac22a65d8b4342a5d8938e93d01ed3c43
vir <virs98@outlook.com>
2025-07-18 11:26:10 +0800

    refactor(主题): 将主题配置拆分为单独文件
    将原本在index.ts中的light和dark主题配置拆分为单独文件，提高代码可维护性和可读性

--- src/components/desktop/themes/index.ts
+++ src/components/desktop/themes/index.ts
@@ -1,3 +1,6 @@
+import { themeDark } from "./dark";
+import { themeLight } from "./light";
+
 export interface Theme {
   token: {
     itemNameColor?: string;
@@ -12,36 +15,6 @@ export interface Theme {
     contextMenuShadowColor?: string;
   };
 }
-
-export const themeLight: Theme = {
-  token: {
-    itemNameColor: '#1a1a1a',
-    itemIconBackgroundColor: 'white',
-    itemIconShadowColor: 'rgba(0, 0, 0, 0.1)',
-    groupItemIconBackgroundColor: 'rgba(255, 255, 255, 0.1)',
-    groupItemIconShadowColor: 'rgba(0, 0, 0, 0.1)',
-    groupItemModalBackgroundColor: 'rgba(255, 255, 255, 0.8)',
-    contextMenuTextColor: 'black',
-    contextMenuActiveColor: '#f3f4f6',
-    contextMenuBackgroundColor: 'white',
-    contextMenuShadowColor: 'rgba(0, 0, 0, 0.1)',
-  },
-};
-
-export const themeDark: Theme = {
-  token: {
-    itemNameColor: 'white',
-    itemIconBackgroundColor: '#1f2937',
-    itemIconShadowColor: 'rgba(0, 0, 0, 0.1)',
-    groupItemIconBackgroundColor: 'rgba(0, 0, 0, 0.1)',
-    groupItemIconShadowColor: 'rgba(0, 0, 0, 0.1)',
-    groupItemModalBackgroundColor: 'rgba(0, 0, 0, 0.1)',
-    contextMenuTextColor: 'white',
-    contextMenuActiveColor: '#1a1a1a',
-    contextMenuBackgroundColor: '#1a1a1a',
-    contextMenuShadowColor: 'rgba(255, 255, 255, 0.1)',
-  },
-};
 
 // 默认主题为浅色主题
 export const defaultTheme = themeLight;
---
+++ src/components/desktop/themes/light.ts
@@ -0,0 +1,16 @@
+import { Theme } from ".";
+
+export const themeLight: Theme = {
+  token: {
+    itemNameColor: "#1a1a1a",
+    itemIconBackgroundColor: "white",
+    itemIconShadowColor: "rgba(0, 0, 0, 0.1)",
+    groupItemIconBackgroundColor: "rgba(255, 255, 255, 0.1)",
+    groupItemIconShadowColor: "rgba(0, 0, 0, 0.1)",
+    groupItemModalBackgroundColor: "rgba(255, 255, 255, 0.8)",
+    contextMenuTextColor: "black",
+    contextMenuActiveColor: "#f3f4f6",
+    contextMenuBackgroundColor: "white",
+    contextMenuShadowColor: "rgba(0, 0, 0, 0.1)",
+  },
+};
