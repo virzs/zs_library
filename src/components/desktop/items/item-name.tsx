cb5e1bd01286dad9f9a4bb8973c28e0d2c28bcb7
vir <virs98@outlook.com>
2025-08-04 16:20:49 +0800

    style(components): 为 item-name 组件添加小字号样式

--- src/components/desktop/items/item-name.tsx
+++ src/components/desktop/items/item-name.tsx
@@ -33,7 +33,7 @@ const ItemName = <D, C>(props: ItemNameProps<D, C>) => {
   return (
     <motion.p
       className={cx(
-        "zs-whitespace-nowrap zs-text-ellipsis zs-overflow-hidden zs-text-center zs-mt-1 zs-mb-0 zs-absolute zs-left-0 zs-right-0",
+        "zs-whitespace-nowrap zs-text-ellipsis zs-overflow-hidden zs-text-center zs-mt-1 zs-mb-0 zs-absolute zs-left-0 zs-right-0 zs-text-sm",
         css`
           color: ${theme.token.items?.textColor};
         `,
